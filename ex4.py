639aaf99e9445a5d8322a90f1c53f415f8363c3d
Nicholas Kier <nicholas.k.kier@gmail.com>
2015-10-14 09:44:46 -0400

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,17 @@
+cars = 100 #sets a variable
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers #100-30
+cars_driven = drivers 
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car"
+
+#The _ in "space_in_a_car" is called an underscore character.
